f0071ccff51a84064bc2d3b176195d7ad1f6892f
Divya <karan.boss1000@gmail.com>
2023-11-13 09:02:47 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import React from "react";
-import Homepage from "./components/homepage";
-import About from "./components/about";
-import Articles from "./components/articles";
-import Footer from "./components/footer";
+// import Homepage from "./components/homepage";
+// import About from "./components/about";
+// import Articles from "./components/articles";
+// import Footer from "./components/footer";
+import lab from "./assets/lab.txt"
 
 function App() {
-  return (
+  return(
     <div className="App">
-      <Homepage />
+      {/* <Homepage />
       <About />
       <Articles />
-      <Footer />
+      <Footer /> */}
+      <a href={lab} download="lab.txt">
+          Download
+      </a>
     </div>
   );
 }
